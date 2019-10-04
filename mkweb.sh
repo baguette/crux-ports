812669958628b52ce875fa14cc5d60a18b0f7609
Cory Burgett <cmburget@gmail.com>
2019-10-03 21:16:09 -0400

    lots of long-overdue updates

--- mkweb.sh
+++ mkweb.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 find . -maxdepth 2 -type f -name Pkgfile -execdir sh -c '
-	printf "%s\\t" "${PWD##*/}"
-	sed -nre '\''s/^#\s*URL\S*\s+(.*)$/\1/p'\'' "$1" || echo "$1"
+	printf "%s" "${PWD##*/}"
+	sed -nre '\''
+		/^version=/ {s/^version=(\S+)/\t\1/;G;s/\n//p}
+		/^#\s*URL/  {s/^#\s*\S+\s+(.*)$/\t\1/;h}
+	'\'' "$1" || echo "$1"
 ' -- {} \; \
+| sort -k1,1 \
 | awk '
 	BEGIN {print "<html><head><title>port links</title></head><body><table>"}
-	{printf "\t<tr><td>%s</td><td><a href=\"%s\">%s</a></td></tr>\n", $1, $2, $2}
+	{
+		printf "\t<tr>"
+		printf "<td>%s</td>", $1
+		printf "<td>%s</td>", $2
+		printf "<td><a href=\"%s\">%s</a></td>", $3, $3
+		printf "</tr>\n"
+	}
 	END {print "</table></body></html>"}
 '
